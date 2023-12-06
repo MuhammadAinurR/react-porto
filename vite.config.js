19c8ee9b06e99a9799f9fb20a0ef64cfee382492
Muhammad Ainur Rofiq <muhammadainurrofiq@Muhammads-MacBook-Air.local>
2023-12-06 09:36:04 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-import viteCompression from "vite-plugin-compression";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteCompression({
-      algorithm: "gzip", // Use gzip compression
-      deleteOriginFile: true, // Delete the original uncompressed file after compressing
-    }),
-  ],
+  plugins: [react()],
 });
