8bf5e15767bac8494c7e4c24344ce3fcadb0aae6
Muhammad Ainur Rofiq <muhammadainurrofiq@Muhammads-MacBook-Air.local>
2023-12-11 23:36:19 +0700

    m

--- src/api/crud.js
+++ src/api/crud.js
@@ -4,23 +4,31 @@ import { useState, useEffect } from "react";
 
 function Crud() {
   const [projectList, setProjectList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const projectsCollectionRef = collection(db, "projects");
+
   useEffect(() => {
+    // console.log("test");
+
     const getProjectList = async () => {
+      setIsLoading(true);
       try {
         const data = await getDocs(projectsCollectionRef);
         const filteredData = data.docs.map((doc) => ({
           ...doc.data(),
         }));
         setProjectList(filteredData);
+        setIsLoading(false);
       } catch (err) {
         console.error(err);
+        setIsLoading(false);
       }
     };
 
     getProjectList();
   }, []);
-  return projectList;
+
+  return [projectList, isLoading];
 }
 
 export { Crud };
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
-
+import { useState, useEffect } from "react";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
 import { Crud } from "../api/crud";
 import { fadeIn, textVariant } from "../utils/motion";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ProjectCard = ({
   index,
@@ -17,69 +19,75 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="flex flex-wrap h-[400px]">
-          <div className="relative w-full h-[230px]">
-            <img
-              src={image}
-              alt="project_image"
-              className="w-full h-full object-cover rounded-2xl"
-            />
+    // <motion.div variants={fadeIn("up", index * 0.5, 0.75)}>
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+    >
+      <div className="flex flex-wrap h-[400px]">
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
 
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover ">
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  src={github}
-                  alt="source code"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-              </div>
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover ">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
             </div>
           </div>
-
-          <div className="relative w-full h-[150px] overflow-auto mx-0.5">
-            <h3 className="text-white font-bold text-[24px]">{name}</h3>
-            <p className="mt-2 text-secondary text-[14px] text-justify ">
-              {description}
-            </p>
-          </div>
-
-          <div className=" flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <p
-                key={`${name}-${tag.name}`}
-                className={`text-[14px] font-medium overflow-auto text-${tag.color}-500`}
-              >
-                #{tag.name}
-              </p>
-            ))}
-          </div>
         </div>
-      </Tilt>
-    </motion.div>
+
+        <div className="relative w-full h-[150px] overflow-auto mx-0.5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px] text-justify ">
+            {description}
+          </p>
+        </div>
+
+        <div className=" flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] font-medium overflow-auto text-${tag.color}-500`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </div>
+    </Tilt>
+    // </motion.div>
   );
 };
 
 const Works = () => {
-  const projects = Crud();
+  // const [isLoading, setIsLoading] = useState(false);
+  const [projects, isLoading] = Crud();
+  console.log(projects);
+
+  // useEffect(() => {
+  //   projects.
+  // })
+
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>My work</p>
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
-
       <div className="w-full flex">
         <motion.p
           variants={fadeIn("", "", 0.1, 1)}
@@ -93,11 +101,19 @@ const Works = () => {
         </motion.p>
       </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
-        ))}
-      </div>
+      {isLoading ? (
+        <SkeletonTheme baseColor="#202020" highlightColor="#444">
+          <p>
+            <Skeleton count={3} />
+          </p>
+        </SkeletonTheme>
+      ) : (
+        <div className="mt-20 flex flex-wrap gap-7">
+          {projects.map((project, index) => (
+            <ProjectCard key={`project-${index}`} index={index} {...project} />
+          ))}
+        </div>
+      )}
     </>
   );
 };
